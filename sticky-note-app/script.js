dcc93f5746b1c0daeb6302a5538d1c3a9409ec5c
ScriptCat000 <scriptcat000@gmail.com>
2025-05-18 16:32:51 +0000

    a sticky note app made by me.(without any help)

--- sticky-note-app/script.js
+++ sticky-note-app/script.js
@@ -6,7 +6,37 @@ addBtn.addEventListener('click', ()=> {
     if(inputBox.value === ""){
         alert("You must write something!");
     }
-    let li = document.createElement("li");
-    li.textContent = "Fardin";
-    noteContainer.appendChild(li);
+    else {
+        let li = document.createElement("li");
+        let paragraph = document.createElement("p");
+        let span = document.createElement("span")
+        
+        li.appendChild(paragraph);
+        li.appendChild(span);
+
+        paragraph.textContent = inputBox.value;
+        span.textContent = "delete";
+
+        noteContainer.appendChild(li);
+    }
+
+    inputBox.value = "";
+    saveNote();
 });
+
+noteContainer.addEventListener('click', (e)=> {
+    if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveNote();
+    };
+});
+
+function saveNote(){
+    localStorage.setItem("notes", noteContainer.innerHTML);
+};
+
+function showNotes(){
+    noteContainer.innerHTML = localStorage.getItem("notes")
+}
+
+showNotes();
